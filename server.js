f889cbca59c970b730b79dd1aa19ef92137e1bcf
Tehman700 <tehmanfast@gmail.com>
2025-10-10 12:33:58 +0500

    This commit is before the 2nd contract

--- server.js
+++ server.js
@@ -5201,8 +5201,6 @@ function generateEmailContent(userName, assessmentType, reportData) {
     </html>
   `;
 }
-
-
 // ----------------------------
 app.listen(process.env.PORT, () =>
   console.log(`🚀 Server running on port ${process.env.PORT}`)
