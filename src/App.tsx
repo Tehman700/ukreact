603a7d51454a2a1b7165c4f795eaa88e4f4d5015
Tehman700 <tehmanfast@gmail.com>
2025-12-12 20:07:23 +0500

    Pulling New Changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { CookieConsent } from './components/CookieConsent';
 import { DesignSystemApp } from './components/design-system/DesignSystemApp';
 import { ShoppingBasket } from './components/ShoppingBasket';
 
-
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
 import { ServicesPage } from './pages/ServicesPage';
